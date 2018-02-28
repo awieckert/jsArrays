192263f4fb667c720c895d44b7388502a1edeeb8
Adam Wieckert <wiec1369@gmail.com>
2018-02-27 21:18:21 -0600

    Made some for loops and stuff.

--- arrays.js
+++ arrays.js
@@ -14,3 +14,25 @@ var palindrome = function (value){
 palindrome("racecar");
 
 
+var animals = ["cat", "goat", "hip, hop"]
+var animalzElement = document.getElementById('animalz');
+ for(var i = 0; i < animals.length; i++){
+    console.log("animal: ", animals[i]);
+    animalzElement.innerHTML += '<h5>' + animals[i] + '</h5>';
+ }
+
+ var book1 = "Catch 22";
+ var book2 = "Slaughterhouse 5";
+ var book3 = "Fahrenheit 451";
+
+ var bookSplit = book1.split('');
+ var realNumz = [];
+
+ for(var j = 0; j < bookSplit.length; j++){
+      if (bookSplit[j]*1) {
+            realNumz.push(bookSplit[j]);
+      }
+
+ }
+
+ console.log ("answer:", realNumz.join(''));
