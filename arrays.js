34a1eb56fbe76286b5e20626085ff3590cec4f77
Adam Wieckert <wiec1369@gmail.com>
2018-02-27 19:46:38 -0600

    Added a function that splits a string, reverses it, joins it, then compairs it to first string.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,16 @@
 console.log("This is a console Log");
+
+
+
+var palindrome = function (value){
+    var toArray = value.split('').reverse().join('');
+    if (value === toArray) {
+        alert(value + " is a Palindrome!")
+    } else {
+        alert(value + " is not a Palindron!");
+    }
+}
+
+palindrome("racecar");
+
+
